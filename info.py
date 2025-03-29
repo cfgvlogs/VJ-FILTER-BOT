709ae148ef48d91b38f69ded26def88d8c2356c0
cfgvlogs <cfgvlogs@gmail.com>
2025-03-29 16:22:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,11 +13,11 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24638450'))
 API_HASH = environ.get('API_HASH', 'b720cf2f8cd64bc0ecee7ea3652a1cd1')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7915694603:AAE9M2bRin1o08SYGXhj6HTE17jmkEouROo")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://ibb.co/0j3tNyXn https://ibb.co/zVFf4Qcf https://ibb.co/Vcy4tzjC https://ibb.co/1Y5vfMsX')).split()
 
 
 # Admins & Users
@@ -26,10 +26,10 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002595022779'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1002595022779'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002374193522').split()]  # For Multiple Id Use One Space Between Each.
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002374193522 1002406572500').split()]  # For Multiple Id Use One Space Between Each.
 
 # auth_channel means force subscribe channel.
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
