d41f8eef6732b158f1e9d74be5a63402529d28d2
daydieanother <daydieanother477@gmail.com>
2025-03-31 12:45:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://makeyouearn.in')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'makeyouearn.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd7c9120bf901ffbfc1d5de306b404f38f56f931a')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/moviesimplyfytuber/28') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
