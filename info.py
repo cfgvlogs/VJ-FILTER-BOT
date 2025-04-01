b13946e38ece55a9a5a190dc1883bafd0a3df8d0
cfgvlogs <cfgvlogs@gmail.com>
2025-04-01 13:01:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,9 +125,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'sharedisklinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '33d3238d4b28af86ab0d3d6846e78e2d63a075c1')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'makeyouearn.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'd7c9120bf901ffbfc1d5de306b404f38f56f931a')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/moviesimplyfytuber/28') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
