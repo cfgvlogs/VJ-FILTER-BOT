5b63db9d16601f74bbbca5b67e5401f65a25d888
cfgvlogs <cfgvlogs@gmail.com>
2025-04-01 11:21:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '02096eccdd1bb5dbd969b5023a01e694c071c60c)
+SHORTLINK_API = environ.get('SHORTLINK_API', '02096eccdd1bb5dbd969b5023a01e694c071c60c')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/moviesimplyfytuber/28') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
